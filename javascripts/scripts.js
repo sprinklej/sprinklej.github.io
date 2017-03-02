9f75aeac23f810ba3c9c5015c4f7eded73bc1702
Joel Sprinkle <joel.sprinkle@carleton.ca>
2017-03-02 12:14:27 -0500

    code clean up

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,7 +1,46 @@
+// Write out with js to try and avoid bots
 function writeMe() {
 	a='sprinklej';
-	b='gm'; c='ail.';
+	b='gm'; 
+  c='ail.';
 	d='com';
 	document.write('<a'+' hre'+'f="ma'+'i'+'lt'+'o:'+a+'@'+b+c+d+'">');
 	//document.write(a+'</a>');
+}
+
+
+
+// Slideshow functions
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var alts = document.getElementsByClassName("disp");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+  captionText.innerHTML = alts[slideIndex-1].alt;
 }
